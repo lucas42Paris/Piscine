95c637dd5c4634f61bb782b0a231857149d7bc5e
Ontiimeyes <lucassalinroge@gmail.com>
2023-07-15 13:25:28 +0200

    Ready to push

---
+++ C07/ft_ultimate_range.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsalin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/22 11:00:46 by lsalin            #+#    #+#             */
+/*   Updated: 2023/01/19 14:37:22 by lsalin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	i;
+
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	*range = malloc((max - min) * sizeof(int));
+	if (!*range)
+		return (-1);
+	i = 0;
+	while (i < max - min)
+	{
+		(*range)[i] = min + i;
+		i++;
+	}
+	return (i);
+}
